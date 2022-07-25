586002fd8703ba370be4c71a038c959f8ebcfdf3
Medha0312 <106583801+Medha0312@users.noreply.github.com>
2022-07-25 11:30:57 +0530

    Add files via upload

--- StudentService.java
+++ StudentService.java
@@ -24,40 +24,39 @@ public class StudentService {
 		index++;
 		System.out.println("Students has been added successfully");
 	}
-}
+
 	
-//	public static void getStudentById(int id) throws StudentNotFoundException
-//	{
-//		boolean f=false;
-//		for (int i=0;i<index;i++)
-//		{
-//		if(id==student[i].getStudentId())
-//		{
-//			//System.out.println(student[i]);
-//			System.out.println("Student id: "+student[i].getStudentId());
-//			System.out.println("Student Name: "+student[i].getStudentName());
-//			System.out.println("Student Address: "+student[i].getAddress());
-//		f=true;
-//		break;
-//		}
-//		}
-//		if(f==false)
-//			throw new StudentNotFoundException("Student not found with id: "+id);
-//		System.out.println("===================================");
-//	}
-//
-//	public static void getAllStudent() {
-//		
-//		for (int i=0;i<index;i++)
-//		{
-//		
-//			//System.out.println(student[i]);
-//			System.out.println("Student id: "+student[i].getStudentId());
-//			System.out.println("Student Name: "+student[i].getStudentName());
-//			System.out.println("Student Address: "+student[i].getAddress());
-//		
-////		}
-//	}
+public static void getStudentById(int id) throws StudentNotFoundException	{
+		boolean f=false;
+		for (int i=0;i<index;i++)
+		{
+		if(id==student[i].getStudentId())
+		{			//System.out.println(student[i]);
+		System.out.println("Student id: "+student[i].getStudentId());
+			System.out.println("Student Name: "+student[i].getStudentName());
+			System.out.println("Student Address: "+student[i].getAddress());
+		f=true;
+		break;
+		}
+		}
+		if(f==false)
+			throw new StudentNotFoundException("Student not found with id: "+id);
+		System.out.println("===================================");
+	}
+
+	public static void getAllStudent() {
+		
+		for (int i=0;i<index;i++)
+		{
+		
+			//System.out.println(student[i]);
+			System.out.println("Student id: "+student[i].getStudentId());
+			System.out.println("Student Name: "+student[i].getStudentName());
+			System.out.println("Student Address: "+student[i].getAddress());
+		
+		}
+	}
+	}
 
 //     static ArrayList<Student> student=new ArrayList<Student>();
 //     public static void creatStudent(Student s)
